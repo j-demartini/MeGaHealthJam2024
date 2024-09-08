a25ab4e88bc1d6b078ed234309077313e00fcadf
Monstroe <colemontrose@gmail.com>
2024-09-07 20:39:37 -0400

    Latest

--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,18 +4,11 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-
-    private Vector3 rotation;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 rotation;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(rotation * Time.deltaTime);   
+        transform.Rotate(rotation * Time.deltaTime);
     }
 }
